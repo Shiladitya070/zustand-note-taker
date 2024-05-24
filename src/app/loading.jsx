47bdc212eb5ea452b07742da403f6192febafdfe
Shiladitya Das <shiladityad940@gmail.com>
2024-05-25 05:20:17 +0530

    homepage change

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen gap-2">
+      <h1 className="text-2xl font-bold">Loading...</h1>
+    </div>
+  );
+}
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,13 +2,27 @@
 import { useNoteStore } from "@/stores/noteStore";
 
 import SingleNote from "./SingleNote";
+import Link from "next/link";
 
 const NoteList = () => {
   const { notes } = useNoteStore((state) => ({
     notes: state.notes,
   }));
 
-  if (notes.length < 0) return <h1>Empty...</h1>;
+  if (notes.length == 0)
+    return (
+      <div className="flex flex-col justify-start h-screen items-center mt-10">
+        <h1 className="text-2xl font-bold">
+          Hi, welcome to Note taker.
+          <br />
+          It looks empty hit this button
+        </h1>
+        <br />
+        <Link className="bg-slate-300 p-2 rounded-md w-fit" href="/create">
+          Create
+        </Link>
+      </div>
+    );
   return (
     <div className="flex mt-6 flex-wrap">
       {notes.map((note) => (
